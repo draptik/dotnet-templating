bc7860ccb92912de248eeb999508906a187acaf4
Patrick Drechsler <patrick@pdrechsler.de>
2024-01-27 13:33:32 +0100

    🔧 integrate 'tocpm' tool

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,6 +4,7 @@ DEFAULT_PROJECT_NAME="Demo"
 DEFAULT_TARGET_DIR="../out"
 
 RESOURCE_DIR=$(readlink -f "./resources")
+echo "==> RESOURCE_DIR: ${RESOURCE_DIR}"
 
 # if there are no command line argument, use default values
 # otherwise use the command line arguments
@@ -34,22 +35,31 @@ LIBRARY_NAME="${PROJECT_NAME}.MyLib"
 
 # General files ---------------------------------------------------------------
 echo "==> Creating general files in target folder: ${TARGET_DIR_ABSOLUTE_PATH} ..."
-cp "${RESOURCE_DIR}/Directory.Build.props.template" "${TARGET_DIR_ABSOLUTE_PATH}/Directory.Build.props"
-cp "${RESOURCE_DIR}/Directory.Packages.props.template" "${TARGET_DIR_ABSOLUTE_PATH}/Directory.Packages.props"
 cp "${RESOURCE_DIR}/.gitattributes.template" "${TARGET_DIR_ABSOLUTE_PATH}/.gitattributes"
 cp "${RESOURCE_DIR}/.global.json.template" "${TARGET_DIR_ABSOLUTE_PATH}/global.json"
-# TODO add .editorconfig
-echo "==> Created general files in target folder: $(pwd)"
+mkdir "${TARGET_DIR_ABSOLUTE_PATH}/.config"
+cp "${RESOURCE_DIR}/.config/dotnet-tools.json" "${TARGET_DIR_ABSOLUTE_PATH}/.config/dotnet-tools.json"
 
-echo "==> Creating solution: ${PROJECT_NAME} in folder ${TARGET_DIR_ABSOLUTE_PATH} ..."
+cp "${RESOURCE_DIR}/Directory.Build.props.template" "${TARGET_DIR_ABSOLUTE_PATH}/Directory.Build.props"
+# cp "${RESOURCE_DIR}/Directory.Packages.props.template" "${TARGET_DIR_ABSOLUTE_PATH}/Directory.Packages.props"
+
+# TODO add .editorconfig
+echo "==> Created general files in target folder: ${TARGET_DIR_ABSOLUTE_PATH}"
+
+echo "==> Creating solution: '${PROJECT_NAME}' in folder '${TARGET_DIR_ABSOLUTE_PATH}'..."
 dotnet new sln -n "${PROJECT_NAME}" -o "${TARGET_DIR_ABSOLUTE_PATH}"
-echo "==> Created solution: $(pwd)"
+echo "==> Created solution: '${PROJECT_NAME}' in folder '${TARGET_DIR_ABSOLUTE_PATH}'"
 
 # Src folder ------------------------------------------------------------------
 echo "==> Creating src folder: ${SRC_DIR} ..."
 mkdir "${SRC_DIR}"
 cd "${SRC_DIR}" || exit
 echo "==> Created src folder: $(pwd)"
+
+# Add Directory.Build.props to src folder
+echo "==> Adding Directory.Build.props to src folder ..."
+cp "${RESOURCE_DIR}/src/Directory.Build.props.template" "${TARGET_DIR_ABSOLUTE_PATH}/src/Directory.Build.props"
+echo "==> Added Directory.Build.props to src folder"
 
 # Create Lib project
 echo "==> Creating a class library: ${LIBRARY_NAME} ..."
@@ -61,15 +71,15 @@ echo "==> Creating a webapi project: ${WEBAPI_NAME} ..."
 dotnet new webapi --no-restore --use-program-main --use-controllers -n "${WEBAPI_NAME}"
 echo "==> Created a webapi project: ${WEBAPI_NAME}"
 
-# The default webapi template doesn't know anything about 
-# the Central Package Management feature ('Directory.*.props').
-# We manually have to "cleanup".
-# We remove the version string from package references within the webapi project file:
-echo "==> Cleaning up webapi project file: ${WEBAPI_NAME}.csproj ..."
-sed -i \
-    's/\(<PackageReference Include="[^"]*" \)Version="[^"]*" /\1/' \
-    "${WEBAPI_NAME}/${WEBAPI_NAME}.csproj"
-echo "==> Cleaned up webapi project file: ${WEBAPI_NAME}.csproj"
+# # The default webapi template doesn't know anything about 
+# # the Central Package Management feature ('Directory.*.props').
+# # We manually have to "cleanup".
+# # We remove the version string from package references within the webapi project file:
+# echo "==> Cleaning up webapi project file: ${WEBAPI_NAME}.csproj ..."
+# sed -i \
+#     's/\(<PackageReference Include="[^"]*" \)Version="[^"]*" /\1/' \
+#     "${WEBAPI_NAME}/${WEBAPI_NAME}.csproj"
+# echo "==> Cleaned up webapi project file: ${WEBAPI_NAME}.csproj"
 
 # WebApi depends on Lib
 dotnet add "${WEBAPI_NAME}" reference "${LIBRARY_NAME}"
@@ -78,14 +88,19 @@ dotnet add "${WEBAPI_NAME}" reference "${LIBRARY_NAME}"
 mkdir "${TEST_DIR}"
 cd "${TEST_DIR}" || exit
 
+# # Add Directory.Build.props to tests folder
+# echo "==> Adding Directory.Build.props to tests folder ..."
+# cp "${RESOURCE_DIR}/tests/Directory.Build.props.template" "${TARGET_DIR_ABSOLUTE_PATH}/tests/Directory.Build.props"
+# echo "==> Added Directory.Build.props to tests folder"
+
 # Create Lib.Tests project
 LIBRARY_TEST_NAME="${LIBRARY_NAME}.Tests"
 dotnet new xunit --no-restore -n "${LIBRARY_TEST_NAME}"
-# Remove the version string from package references within the test project file
-# Also remove the inner text of the <PackageReference> element
-sed -i \
-    '/<PackageReference Include="[^"]*" /,/<\/PackageReference>/ {/Version="[^"]*"/s///; /<PackageReference/s/>[^<]*</></;}' \
-    "${LIBRARY_TEST_NAME}/${LIBRARY_TEST_NAME}.csproj"
+# # Remove the version string from package references within the test project file
+# # Also remove the inner text of the <PackageReference> element
+# sed -i \
+#     '/<PackageReference Include="[^"]*" /,/<\/PackageReference>/ {/Version="[^"]*"/s///; /<PackageReference/s/>[^<]*</></;}' \
+#     "${LIBRARY_TEST_NAME}/${LIBRARY_TEST_NAME}.csproj"
 
 # Lib.Tests depends on Lib
 dotnet add "${LIBRARY_TEST_NAME}" reference "${SRC_DIR}/${LIBRARY_NAME}"
@@ -99,5 +114,28 @@ dotnet sln add "${SRC_DIR}/${LIBRARY_NAME}"
 dotnet sln add "${SRC_DIR}/${WEBAPI_NAME}"
 dotnet sln add "${TEST_DIR}/${LIBRARY_TEST_NAME}"
 
+# Restore dotnet tools
+echo "==> Restoring dotnet tools ..."
+dotnet tool restore
+echo "==> Restored dotnet tools"
+
+# Restore nuget packages
+echo "==> Restoring nuget packages ..."
 dotnet restore
-dotnet test
+echo "==> Restored nuget packages"
+
+# Run test
+# echo "==> Running tests ..."
+# dotnet test
+# echo "==> Ran tests"
+
+echo "==> Running tocpm ... (when asked, select 'y' or 'enter', then press Ctrl+C to end tocpm)"
+dotnet tool run tocpm execute .
+echo "==> Ran tocpm"
+
+# Upgrade nuget packages
+echo "==> Upgrading nuget packages ..."
+dotnet outdated --upgrade
+echo "==> Upgraded nuget packages"
+echo "==>"
+echo "==> Done"
