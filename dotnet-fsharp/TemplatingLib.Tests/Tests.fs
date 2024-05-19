e1fa2af689fcc68a6835f4ac0dc4b6f2fa5c4300
Patrick Drechsler <patrick@pdrechsler.de>
2024-05-19 19:45:37 +0200

    ✨ use FsToolkit.Errorhandling's validation CE

--- dotnet-fsharp/TemplatingLib.Tests/Tests.fs
+++ dotnet-fsharp/TemplatingLib.Tests/Tests.fs
@@ -1,7 +1,106 @@
 module Tests
 
+open TemplatingLib.Io
 open Xunit
+open Swensen.Unquote
+
+let isOk result =
+    match result with
+    | Ok _ -> true =! true
+    | Error _ -> true =! false
+
+let isError result =
+    match result with
+    | Ok _ -> true =! false
+    | Error _ -> true =! true
+
+let hasErrors (errorsExpected: ApplicationError list) (results: Result<unit, ApplicationError list>) =
+    match results with
+    | Ok _ -> true =! false
+    | Error errors -> List.forall (fun e -> List.contains e errorsExpected) errors =! true
+
+let hasError (error: ApplicationError) (results: Result<unit, ApplicationError list>) = hasErrors [ error ] results
+
+let validProjectType = "classlib"
+let validProjectName = "Foo"
+let validPath = "~/tmp/foo"
+let validLanguage = "c#"
 
 [<Fact>]
-let ``create project`` () =
-    TemplatingLib.Io.createDotnetProject "classlib" "Foo" "~/tmp/foo" "c#"
+let ``create project - happy case w/ linux`` () =
+    let actual =
+        createDotnetProject validProjectType validProjectName validPath validLanguage
+
+    actual |> isOk
+
+[<Fact>]
+let ``create project - invalid project type`` () =
+    let invalidProjectType = "invalidProjectType"
+
+    let actual =
+        createDotnetProject invalidProjectType validProjectName validPath validLanguage
+
+    let expected = (UnknownProjectType invalidProjectType)
+    actual |> hasError expected
+
+[<Fact>]
+let ``create project - invalid project name`` () =
+    let invalidProjectName = ""
+
+    let actual =
+        createDotnetProject validProjectType invalidProjectName validPath validLanguage
+
+    let expected = (InvalidProjectName "Project name must not be empty")
+    actual |> hasError expected
+
+[<Fact>]
+let ``create project - invalid path / empty`` () =
+    let invalidPath = ""
+
+    let actual =
+        createDotnetProject validProjectType validProjectName invalidPath validLanguage
+
+    let expected =
+        (CantCreateOutputDirectory "The value cannot be an empty string. (Parameter 'path')")
+
+    actual |> hasError expected
+
+[<Fact>]
+let ``create project - invalid path / access denied - linux`` () =
+    let invalidPath = "/doesnotexist"
+
+    let actual =
+        createDotnetProject validProjectType validProjectName invalidPath validLanguage
+
+    let expected =
+        (CantCreateOutputDirectory $"Access to the path '%s{invalidPath}' is denied.")
+
+    actual |> hasError expected
+
+[<Fact>]
+let ``create project - invalid language`` () =
+    let invalidLanguage = "vb.net"
+
+    let actual =
+        createDotnetProject validProjectType validProjectName validPath invalidLanguage
+
+    let expected = (UnknownLanguage invalidLanguage)
+    actual |> hasError expected
+
+[<Fact>]
+let ``create project - all inputs invalid`` () =
+    let invalidProjectType = "invalidProjectType"
+    let invalidProjectName = ""
+    let invalidPath = "/doesnotexist"
+    let invalidLanguage = "vb.net"
+
+    let actual =
+        createDotnetProject invalidProjectType invalidProjectName invalidPath invalidLanguage
+
+    let expected =
+        [ (UnknownLanguage invalidLanguage)
+          (CantCreateOutputDirectory $"Access to the path '%s{invalidPath}' is denied.")
+          (InvalidProjectName "Project name must not be empty")
+          (UnknownProjectType invalidProjectType) ]
+
+    actual |> hasErrors expected
--- dotnet-fsharp/TemplatingLib/Library.fs
+++ dotnet-fsharp/TemplatingLib/Library.fs
@@ -2,14 +2,70 @@
 
 open System.Diagnostics
 open System.IO
+open FsToolkit.ErrorHandling
 
 module Io =
 
     type ApplicationError =
+        | InvalidProjectName of string
+        | UnknownProjectType of string
+        | UnknownLanguage of string
         | CantCreateOutputDirectory of string
         | CantCreateDotnetProject of string
 
     type ValidatedPath = ValidatedPath of string
+
+    type ProjectType = | ClassLib
+
+    let tryConvertToProjectType (s: string) =
+        match s with
+        | "classlib" -> ClassLib |> Ok
+        | e -> Error(UnknownProjectType e)
+
+    let convertProjectTypeToString =
+        function
+        | ClassLib -> "classlib"
+
+    type Language =
+        | CSharp
+        | FSharp
+
+    let tryConvertToLanguage (s: string) =
+        match s with
+        | "c#" -> CSharp |> Ok
+        | "f#" -> FSharp |> Ok
+        | e -> Error(UnknownLanguage e)
+
+    let convertLanguageToString =
+        function
+        | CSharp -> "c#"
+        | FSharp -> "f#"
+
+    type ValidProjectName = private ValidProjectName of string
+
+    module ValidProjectName =
+        let create (name: string) =
+            if name.Length > 0 then
+                ValidProjectName name |> Ok
+            else
+                Error(InvalidProjectName "Project name must not be empty")
+
+        let value (ValidProjectName name) = name
+
+
+    // type ProjectCreationInputs = ValidProjectName * ProjectType * Language * ValidatedPath
+    type ProjectCreationInputs =
+        { ProjectName: ValidProjectName
+          ProjectType: ProjectType
+          Language: Language
+          Path: ValidatedPath }
+
+    let unwrapProjectCreationInputs (inputs: ProjectCreationInputs) =
+        let projectType = convertProjectTypeToString inputs.ProjectType
+        let projectName = ValidProjectName.value inputs.ProjectName
+        let (ValidatedPath path) = inputs.Path
+        let language = convertLanguageToString inputs.Language
+        (projectName, projectType, language, path)
 
     let tryToCreateOutputDirectory (unvalidatedPath: string) : Result<ValidatedPath, ApplicationError> =
         try
@@ -19,28 +75,38 @@ module Io =
         with e ->
             Error(CantCreateOutputDirectory e.Message)
 
-    let tryToCreateDotnetProject
-        (projectType: string)
-        (name: string)
-        (validatedPath: ValidatedPath)
-        (language: string)
-        : Result<unit, ApplicationError> =
+    let tryToCreateDotnetProject (projectCreationInputs: ProjectCreationInputs) : Result<unit, ApplicationError> =
         try
-            let (ValidatedPath path) = validatedPath
-            let pt = projectType.ToString().ToLower()
+            let name, projectType, lang, path =
+                unwrapProjectCreationInputs projectCreationInputs
 
-            Process.Start("dotnet", $"new %s{pt} --name %s{name} --output %s{path} --language %s{language}")
+            Process.Start("dotnet", $"new %s{projectType} --name %s{name} --output %s{path} --language %s{lang}")
             |> ignore
             |> Ok
         with e ->
             Error(CantCreateDotnetProject e.Message)
 
     let createDotnetProject
-        (projectType: string)
-        (name: string)
-        (path: string)
-        (language: string)
-        : Result<unit, ApplicationError> =
-        path
-        |> tryToCreateOutputDirectory
-        |> Result.bind (fun outputDirectory -> tryToCreateDotnetProject projectType name outputDirectory language)
+        (rawProjectType: string)
+        (rawName: string)
+        (rawPath: string)
+        (rawLanguage: string)
+        : Result<unit, ApplicationError list> =
+
+        let tryValidatingInputs =
+            validation {
+                let! projectName = ValidProjectName.create rawName
+                and! projectType = tryConvertToProjectType rawProjectType
+                and! language = tryConvertToLanguage rawLanguage
+                and! path = tryToCreateOutputDirectory rawPath
+
+                return
+                    { ProjectName = projectName
+                      ProjectType = projectType
+                      Language = language
+                      Path = path }
+            }
+
+        match tryValidatingInputs with
+        | Error e -> Error e
+        | Ok inputs -> inputs |> tryToCreateDotnetProject |> Result.mapError (fun e -> [ e ])
