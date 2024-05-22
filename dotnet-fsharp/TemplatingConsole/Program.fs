e8cfe91df7aa207b151a2793461d2fa7969efcfc
Patrick Drechsler <patrick@pdrechsler.de>
2024-05-22 02:25:56 +0200

    ♻️ refactor: extract cli arguments

--- dotnet-fsharp/TemplatingConsole/Program.fs
+++ dotnet-fsharp/TemplatingConsole/Program.fs
@@ -4,21 +4,8 @@ open System
 open System.IO
 open Argu
 
+open TemplatingConsole.Arguments
 open TemplatingLib.Io
-
-type CliArguments =
-    | [<AltCommandLine("-n"); Unique>] Solution_Name of name: string
-    | [<AltCommandLine("-o"); Unique>] Output_Directory of path: string
-    | [<AltCommandLine("-r"); Unique>] Resource_Directory of path: string
-    | [<AltCommandLine("-f"); Unique>] Force of bool // TODO
-
-    interface IArgParserTemplate with
-        member s.Usage =
-            match s with
-            | Solution_Name _ -> "The name of the solution to create"
-            | Output_Directory _ -> "The directory where the project will be created"
-            | Resource_Directory _ -> "The directory where the resources are located"
-            | Force _ -> "Force overwrite of existing files"
 
 let errorHandler =
     ProcessExiter(
