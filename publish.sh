23421aec95680efc53761dfad6d8295e0542d8a6
Patrick Drechsler <patrick@pdrechsler.de>
2024-05-27 23:57:49 +0200

    ✨ add initial publish script

---
+++ publish.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+APPLICATION_NAME="TemplatingConsole"
+TARGET_FOLDER="$HOME/tmp/templating-out"
+DOTNET_SRC_FOLDER="dotnet-fsharp"
+
+dotnet publish \
+	--configuration Release \
+	--runtime linux-x64 \
+	--self-contained \
+	--framework net8.0 \
+	-p PublishSingleFile=true \
+	-p DebugType=None \
+	-p DebugSymbols=false \
+	--output "${TARGET_FOLDER}" \
+	"${DOTNET_SRC_FOLDER}/src/${APPLICATION_NAME}"
